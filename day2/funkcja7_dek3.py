569c97adb21052d34b0d5955e543300a7e404643
rajkonkret <rajkonkret660@gmail.com>
2026-06-16 13:42:29 +0200

    [2 32] update funkcja6_dek2.py

--- day2/funkcja7_dek3.py
+++ day2/funkcja7_dek3.py
@@ -2,7 +2,11 @@
 # time.time()
 # time.perf_counter()
 import time
+import numpy as np
 
+
+# pip install numpy
+# numpy biblioteeka do działń na tablicach/macierzach
 
 def measure_time(func):
     def wrapper(*args, **kwargs):
@@ -19,6 +23,9 @@ def measure_time(func):
 
 lista1 = list(range(10_000_000))
 lista2 = list(range(10_000_000))
+
+array1 = np.arange(10_000_000, dtype=np.int64)
+array2 = np.arange(10_000_000, dtype=np.int64)
 
 
 @measure_time
@@ -47,7 +54,14 @@ def add_zip():
     return "OK ZIP"
 
 
+@measure_time
+def add_np():
+    result = array1 + array2  # broadcasting
+    return "OK np"
+
+
 my_time()  # Czas wykonania funkcji: my_time: 2.0009553999989294
 add_with_for()  # Czas wykonania funkcji: add_with_for: 2.68576409999514, Czas wykonania funkcji: add_with_for: 1.5666790999821387
 add_lc()  # Czas wykonania funkcji: add_lc: 1.2878811999689788
 add_zip()  # Czas wykonania funkcji: add_zip: 1.2108676999923773
+add_np()  # Czas wykonania funkcji: add_np: 0.046257699956186116
