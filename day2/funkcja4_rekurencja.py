fbaa49a3b44018d73f3d9149d5a88d9a1866d141
rajkonkret <rajkonkret660@gmail.com>
2026-06-16 11:40:50 +0200

    [2 17] update funkcja3.py.py

---
+++ day2/funkcja4_rekurencja.py
@@ -0,0 +1,51 @@
+# funkcja rekurencyjna
+# funkcja wywołuje sama siebie
+from functools import lru_cache
+
+
+def silnia(n):
+    if n == 0:
+        return 1
+    else:
+        return n * silnia(n - 1)
+
+
+print(silnia(5))  # 120
+
+
+def nwd(a, b):
+    if b == 0:
+        return a
+    return nwd(b, a % b)  # % - modulo, reszta z dzielenia
+
+
+print(nwd(48, 18))  # 6
+
+
+@lru_cache(maxsize=512)
+def fibonacci(n):
+    if n < 0:
+        raise ValueError("n nie może być ujemne")
+
+    if n < 2:
+        return n
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+print(fibonacci(5))
+print(fibonacci.cache_info())
+# CacheInfo(hits=3, misses=6, maxsize=512, currsize=6)
+print(fibonacci(10))
+print(fibonacci.cache_info())
+# CacheInfo(hits=9, misses=11, maxsize=512, currsize=11)
+
+# zsumowac wszystkie wartosci ze słownika
+nested_data = {
+    "a": 1,
+    "b": {
+        "c": 2,
+        "d": [3, 4, {"e": 5}]
+    },
+    "f": [6, 7]
+}
