cd1030b887bd95c296678b24ab962a9bc80050ed
rajkonkret <rajkonkret660@gmail.com>
2026-06-16 15:43:05 +0200

    [2 46] update wycieczka.py

---
+++ day2/wycieczka.py
@@ -0,0 +1,14 @@
+def procent_na_ulamek(fn):
+    def wrapper(*args, **kwargs):
+        pass
+
+    return wrapper
+
+
+@procent_na_ulamek
+def oblicz_rabat(transport, nocleg, rabat):
+    pass
+
+
+def kwota(transport, nocleg, wyzywienie, wycieczki, ubezpieczenie, inne, rabat=0.0):
+    pass
