e19f627316e8dda1c5a3de15675e4356de4df085
rajkonkret <rajkonkret660@gmail.com>
2026-06-16 14:30:34 +0200

    [2 38] update funkcja9_gen1.py

--- day2/funkcja10_gen2.py
+++ day2/funkcja10_gen2.py
@@ -40,3 +40,49 @@ for i in wznowienie(6, 8):
 # Yield zwraca wartość: 144
 # Wznowienie działania - dzielenie
 # Yield zwraca wartość: 2.25
+
+print(20 * "-")
+
+
+def gen4():
+    i = 1
+    while True:
+        yield i * i
+        i += 1
+
+
+generator4 = gen4()
+print(next(generator4))
+print(next(generator4))
+print(next(generator4))
+print(next(generator4))
+print(next(generator4))
+
+
+def gen5():
+    i = 1
+    while True:
+        command = yield i * i
+        print(command)  # None, OK
+        if command == "OK":
+            print("Jestem generatorem")
+        elif command == "stop":
+            break  # zatrzymanie generatora
+        i += 1
+
+
+g5 = gen5()
+print(next(g5))
+print(next(g5))
+print(next(g5))
+g5.send("OK")
+try:
+    g5.send("stop")  # stop, StopIteration
+except StopIteration:
+    print("Koniec danych")
+except Exception as e:
+    print("bład:", e)
+
+# Koniec danych
+
+g5.close()  # zatrzymanie generatora, wskazanie do gc by usunął
