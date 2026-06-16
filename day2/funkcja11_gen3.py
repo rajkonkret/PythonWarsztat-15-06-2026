c07450758c83d80ba796cbda5ab1b63c137dc04e
rajkonkret <rajkonkret660@gmail.com>
2026-06-16 14:58:39 +0200

    [2 41] update funkcja10_gen2.py

---
+++ day2/funkcja11_gen3.py
@@ -0,0 +1,23 @@
+def count_down(min):
+    count = min
+    while count > 0:
+        yield count
+        count -= 1
+
+
+def count_up_to(max):
+    count = 1
+
+    while count <= max:
+        yield count
+        count += 1
+
+
+c_d = count_down(3)
+c_u = count_up_to(3)
+
+for n in c_u:
+    print(n)
+
+for n in c_d:
+    print(n)
