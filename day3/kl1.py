1cb7b03f43cbe2bc90911d4f7cb88cfd0910ab4d
rajkonkret <rajkonkret660@gmail.com>
2026-06-17 11:30:27 +0200

    [3 17] update kl1

--- day3/kl1.py
+++ day3/kl1.py
@@ -19,12 +19,35 @@ class MyFirstClass:
         :param y:
         """
         # self - obiekt
+        # self.x = x
+        # self.y = y
+        self.move(x, y)
+
+    # move(x, y)
+    def move(self, x, y):
+        """
+        Zmienia x i y obiektu na nowe waartości
+        :param x:
+        :param y:
+        :return:
+        """
         self.x = x
         self.y = y
 
+    # metoda opisowa obiektu
+    def __str__(self):
+        return f"{self.x, self.y}"
+
 
 ob = MyFirstClass()
-print(ob) # <__main__.MyFirstClass object at 0x000001DC7FA7CC20>
+print(ob)  # <__main__.MyFirstClass object at 0x000001DC7FA7CC20>
 print(MyFirstClass.__doc__)
 # pydoc -b - serwer
 # pydoc -w .\kl1.py - plik html z dokumentacją
+
+print(ob.x)
+print(ob.y)
+print(ob)  # (0, 0) po nadpisaniu __str__
+
+point1 = MyFirstClass(5, 9)
+print(point1)  # (5, 9)
