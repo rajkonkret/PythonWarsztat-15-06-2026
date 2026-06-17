2d93886d362595a719de6686fae350ad328e981b
rajkonkret <rajkonkret660@gmail.com>
2026-06-17 10:39:27 +0200

    [3 14] update test

--- day3/tests/test_transakcje.py
+++ day3/tests/test_transakcje.py
@@ -39,6 +39,31 @@ def test_filter_transactions_income():
 
     assert tr.filter_transactions(tr.transactions, "income") == expected_list
 
+
 # day3/tests/test_transakcje.py::test_maap_transactions_usd PASSED         [ 33%]
 # day3/tests/test_transakcje.py::test_reduce_transactions PASSED           [ 66%]
 # day3/tests/test_transakcje.py::test_filter_transactions_income PASSED    [100%]
+
+
+# testy parametryzowane
+import pytest
+
+
+@pytest.mark.parametrize(
+    "kind,currency,expected",
+    [
+        ("income", "USD", 1000 + 500 + 700),
+        ("income", "EUR", 100),
+        ("expense", "USD", 200 + 300),
+        ("expense", 'EUR', 400)
+    ]
+)
+def test_process_transactions_param(kind, currency, expected):
+    assert tr.process_transactions(tr.transactions, kind, currency) == expected
+# day3/tests/test_transakcje.py::test_maap_transactions_usd PASSED         [ 14%]
+# day3/tests/test_transakcje.py::test_reduce_transactions PASSED           [ 28%]
+# day3/tests/test_transakcje.py::test_filter_transactions_income PASSED    [ 42%]
+# day3/tests/test_transakcje.py::test_process_transactions_param[income-USD-2200] PASSED [ 57%]
+# day3/tests/test_transakcje.py::test_process_transactions_param[income-EUR-100] PASSED [ 71%]
+# day3/tests/test_transakcje.py::test_process_transactions_param[expense-USD-500] PASSED [ 85%]
+# day3/tests/test_transakcje.py::test_process_transactions_param[expense-EUR-400] PASSED [100%]
