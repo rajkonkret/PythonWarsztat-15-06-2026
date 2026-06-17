f94829b27c82ae2f4ed0f254b6727fbf8fb98d68
rajkonkret <rajkonkret660@gmail.com>
2026-06-17 13:43:38 +0200

    [3 28] update kl5

---
+++ day3/kl5.py
@@ -0,0 +1,32 @@
+# longest_key() - ma wyszukac najdłuższy klucz
+
+class LongestKeyDict(dict):
+
+    def longest_key(self):
+        longest = None
+        for key in self:
+            if longest is None or len(key) > len(longest):
+                longest = key
+
+        return longest
+
+
+art = LongestKeyDict()
+art['tomasz'] = 12
+art['abraham'] = 14
+art['zen'] = 9
+
+print(art.longest_key())  # abraham
+
+
+class LongestKeyDictMax(dict):
+    def longest_key(self):
+        return max(self.keys(), key=len, default=None)
+
+
+art = LongestKeyDictMax()
+art['tomasz'] = 12
+art['abraham'] = 14
+art['zen'] = 9
+
+print(art.longest_key())  # abraham
