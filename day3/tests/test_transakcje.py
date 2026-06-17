554764f29d23b09ccac4116f4fc86dfcd1d17615
rajkonkret <rajkonkret660@gmail.com>
2026-06-17 10:26:07 +0200

    [3 10] update test

--- day3/tests/test_transakcje.py
+++ day3/tests/test_transakcje.py
@@ -2,6 +2,7 @@
 
 x = 5
 assert x == 5
+
 # assert x == 15
 # Traceback (most recent call last):
 #   File "C:\Users\CSComarch\PycharmProjects\PythonWarsztat-15-06-2026\day3\tests\test_transakcje.py", line 5, in <module>
@@ -10,3 +11,9 @@ assert x == 5
 # AssertionError
 
 # map, reduce, filter
+import transakcje as tr  # jako alias
+
+
+def test_maap_transactions_usd():
+    result = [1000, 200, 500, 300, 700, 0, 0]
+    assert tr.map_transactions(tr.transactions, "USD") == result
