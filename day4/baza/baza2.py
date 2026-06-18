d90cd658dd8676ecc50997c66e858a8460c3b1a2
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 12:48:22 +0200

    [4 25] update baza2.py

--- day4/baza/baza2.py
+++ day4/baza/baza2.py
@@ -19,3 +19,10 @@ class SqliteCM:
         if self.conn:
             self.conn.commit()
             self.conn.close()
+
+
+db_name = "sqlite_cm.db"
+
+with SqliteCM(db_name) as conn:
+    cursor = conn.cursor()
+    cursor.execute("CREATE TABLE IF NOT EXISTS users(id INTEGER PRIMARY KEY, name TEXT);")
