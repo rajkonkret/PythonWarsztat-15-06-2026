db50a6a87a5a2ef74fccd567a32b0b6f92c82ed9
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 09:38:46 +0200

    [4 3] update kl10

---
+++ day4/metody_statyczne/ms_zad1.py
@@ -0,0 +1,16 @@
+# metoda statyczna - nie potrzebuja obiektu klasy
+
+class Matematyka:
+
+    # def dodaj(self, a, b):
+    #     return a + b
+
+    @staticmethod
+    def dodaj(a, b):
+        return a + b
+
+
+# kalk = Matematyka()
+# print(kalk.dodaj(56, 90))  # 146
+
+print(Matematyka.dodaj(5, 90))  # 95
