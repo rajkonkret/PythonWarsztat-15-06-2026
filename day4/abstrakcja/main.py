8a7fb5dcc7db5ed0557bfd3d9c2c0f02eccaefe7
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 10:52:44 +0200

    [4 15] update abstrakcja

--- day4/abstrakcja/main.py
+++ day4/abstrakcja/main.py
@@ -7,6 +7,7 @@
 # dorobic klase dziedziczaca po Protyp
 
 from xyz import XYZ
+from regular import Regular
 
 xyz1 = XYZ(1, 2, 3)
 print(xyz1.info("xyz 001"))
@@ -15,3 +16,23 @@ print(f"Policz: {xyz1.policz()}")
 # Policz: 5
 
 xyz1.msg()  # Metoda nieabstrakcyjna klasy abstrakcyjnej
+
+reg1 = Regular(4, 5)
+print(reg1.info("rg 002"))
+print(f"Policz: {reg1.policz()}")
+# Wiadomość: rg 002
+# Policz: 3.2
+
+reg1.msg()
+xyz1.msg()
+# Metoda nieabstrakcyjna klasy abstrakcyjnej
+# Metoda nieabstrakcyjna klasy abstrakcyjnej
+
+lista = [reg1, xyz1]  # obiekty różnych klas
+
+# polimorfizm
+# obiekty róznych kals traktujemy jako obiekty jedej klasy (abstrakcyjne)
+for i in lista:
+    print(i.policz())
+# 3.2
+# 5
