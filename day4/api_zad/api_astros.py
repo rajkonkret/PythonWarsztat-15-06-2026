450f3daf317bd04c9e12837d0e57d895626c4620
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 14:39:57 +0200

    [4 33] update api_astros.py

---
+++ day4/api_zad/api_astros.py
@@ -0,0 +1,10 @@
+# { "name":"John", "age":30, "city":"New York"}
+# REST API
+# Do komunikacji wykorzystywane są standardowe metody HTTP
+# :GET: Pobieranie danych (np. lista użytkowników).
+# POST: Tworzenie nowego zasobu (np. rejestracja konta).
+# PUT / PATCH: Aktualizacja zasobu (całości lub części).
+# DELETE: Usuwanie zasobu.
+# https://github.com/public-apis/public-apis
+
+url = 'http://api.open-notify.org/astros.json'
