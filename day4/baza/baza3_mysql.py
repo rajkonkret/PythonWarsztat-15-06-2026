c32ce0c7c10b92fb3738ede686cfe88fa38a883a
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 13:54:01 +0200

    [4 28] update baza2.py

---
+++ day4/baza/baza3_mysql.py
@@ -0,0 +1,23 @@
+import os
+
+from dotenv import load_dotenv
+import pymysql
+
+load_dotenv()
+
+conn = pymysql.connect(
+    host=os.getenv('DB_HOST'),
+    user=os.getenv('DB_USER'),
+    password=os.getenv('DB_PASSWORD'),
+    database=os.getenv('DB_NAME'),
+    port=int(os.getenv("DB_PORT")),
+)
+
+
+with conn.cursor() as cur:
+    cur.execute("""
+    CREATE TABLE IF NOT EXISTS posts 
+    (id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(100),
+    body VARCHAR(255));""")
+    conn.commit()
