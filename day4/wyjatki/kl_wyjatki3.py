995b6e55dbfcdcb3eaf81babb82a6cd6e207b5bb
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 11:56:57 +0200

    [4 19] update wyjatki

---
+++ day4/wyjatki/kl_wyjatki3.py
@@ -0,0 +1,25 @@
+class MyExceptions(Exception):
+
+    def __init__(self, message):
+        super().__init__(message)
+
+    @staticmethod
+    def sprawdz_wprowadzona_wartosc(wartosc):
+        if wartosc < 0:
+            raise MyExceptions("Liczba musi być większa od zera")
+        if wartosc > 10_000:
+            raise MyExceptions("Liczba za duża")
+
+
+try:
+    x = int(input("Podaj liczbę większą od zera:"))
+    MyExceptions.sprawdz_wprowadzona_wartosc(x)
+except Exception as e:
+    print("Bład:", e)
+else:
+    print("podana wartośc:", x)
+finally:
+    print("koniec")
+# Podaj liczbę większą od zera:-1
+# Bład: Liczba musi być większa od zera
+# koniec
