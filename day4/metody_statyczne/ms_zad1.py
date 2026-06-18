c9729ae0a0a6ec04e7c718eaf1859cbb1551606f
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 09:40:36 +0200

    [4 4] update ms_zad1.py

--- day4/metody_statyczne/ms_zad1.py
+++ day4/metody_statyczne/ms_zad1.py
@@ -9,8 +9,14 @@ class Matematyka:
     def dodaj(a, b):
         return a + b
 
+    @staticmethod
+    def odejmij(a, b):
+        return a - b
+
 
 # kalk = Matematyka()
 # print(kalk.dodaj(56, 90))  # 146
 
 print(Matematyka.dodaj(5, 90))  # 95
+
+print(Matematyka.odejmij(90, 78)) # 12
