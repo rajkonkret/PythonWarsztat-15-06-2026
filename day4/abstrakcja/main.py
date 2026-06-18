a7a85df59b2b71dd94cdf9a70819bccb49f7d15f
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 10:41:11 +0200

    [4 12] update abstrakcja

--- day4/abstrakcja/main.py
+++ day4/abstrakcja/main.py
@@ -2,3 +2,16 @@
 
 # prototyp = Prototyp(1)
 # TypeError: Can't instantiate abstract class Prototyp without an implementation for abstract methods 'info', 'policz'
+
+# uzyc xyz w main
+# dorobic klase dziedziczaca po Protyp
+
+from xyz import XYZ
+
+xyz1 = XYZ(1, 2, 3)
+print(xyz1.info("xyz 001"))
+print(f"Policz: {xyz1.policz()}")
+# xyz 001xyz 001xyz 001
+# Policz: 5
+
+xyz1.msg()  # Metoda nieabstrakcyjna klasy abstrakcyjnej
