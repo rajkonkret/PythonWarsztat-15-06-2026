9a0fdf884ae2e9a9c8ffa762866d636284963fb5
rajkonkret <rajkonkret660@gmail.com>
2026-06-18 15:40:09 +0200

    [4 39] update api_nbp.py

--- day4/selenium_zad1.py
+++ day4/selenium_zad1.py
@@ -17,4 +17,4 @@ data = soup.find('script', {'id': '__NEXT_DATA__'}).text
 print(data)
 
 offers_count = int(data.split('offersCount":')[1].split(",")[0])
-print(offers_count)  # 483
+print(offers_count)  # 379
